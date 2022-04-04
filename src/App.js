1763ac4663e0d7f1ebab6b8f4cfef8b0363ca1e2
Ali Shamsian <Alishamsian504@gmail.com>
2022-04-05 01:44:41 +0430

    header end

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import { Header, MainContainer, CreateContainer } from "./components";
 const App = () => {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
+
+import { initialState } from "./context/initalState";
+import reducer from "./context/reducer";
+import { StateProvider } from "./context/StateProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </BrowserRouter>
 );
