084752f926e12f5848aa277ab269ed07c3a6c1d4
Ali Shamsian <Alishamsian504@gmail.com>
2022-04-09 18:23:51 +0430

    edit end header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,26 +12,12 @@ import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
 
 const Header = () => {
-  const firebaseAuth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-
   const [{ user, cartShow, cartItems }, dispatch] = useStateValue();
 
   const [isMenu, setIsMenu] = useState(false);
 
-  const login = async () => {
-    if (!user) {
-      const {
-        user: { refreshToken, providerData },
-      } = await signInWithPopup(firebaseAuth, provider);
-      dispatch({
-        type: "SET_USER",
-        user: providerData[0],
-      });
-      localStorage.setItem("user", JSON.stringify(providerData[0]));
-    } else {
-      setIsMenu(!isMenu);
-    }
+  const login = () => {
+    setIsMenu(!isMenu);
   };
 
   const logout = () => {
@@ -97,7 +83,9 @@ const Header = () => {
             <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
             {cartItems && cartItems.length > 0 && (
               <div className="absolute -top-2 -right-2 bg-cartNumBg rounded-full w-5 h-5 flex items-center justify-center">
-                <p className="text-xs text-white font-semibold">{cartItems.length}</p>
+                <p className="text-xs text-white font-semibold">
+                  {cartItems.length}
+                </p>
               </div>
             )}
           </div>
@@ -116,13 +104,12 @@ const Header = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
               >
-                {user && user.email === "alishamsian504@gmail.com" && (
-                  <Link to="/createItem">
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-out text-textColor text-base ">
-                      New Item <MdAdd />
-                    </p>
-                  </Link>
-                )}
+                <Link to="/createItem">
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-out text-textColor text-base ">
+                    New Item <MdAdd />
+                  </p>
+                </Link>
+
                 <p
                   className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-out text-textColor text-base "
                   onClick={logout}
@@ -142,10 +129,12 @@ const Header = () => {
             onClick={showCart}
           />
           {cartItems && cartItems.length > 0 && (
-              <div className="absolute -top-2 -right-2 bg-cartNumBg rounded-full w-5 h-5 flex items-center justify-center">
-                <p className="text-xs text-white font-semibold">{cartItems.length}</p>
-              </div>
-            )}
+            <div className="absolute -top-2 -right-2 bg-cartNumBg rounded-full w-5 h-5 flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">
+                {cartItems.length}
+              </p>
+            </div>
+          )}
         </div>
         <Link to="/" className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo " />
